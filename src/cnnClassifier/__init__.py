b4f15a9d4db88a54969cd41f1f06fae938f7c099
Dharssini <dharssinikarthikeyan@gmail.com>
2024-07-13 15:53:02 +0530

    added common functionalities

---
+++ src/cnnClassifier/__init__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import logging
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),  #this for adding logs to logFile
+        logging.StreamHandler(sys.stdout)  #this is for outputting the logs
+    ]
+)
+
+logger = logging.getLogger("cnnClassifierLogger")
---
+++ src/cnnClassifier/utils/__init__.py
@@ -0,0 +1,4 @@
+#utils are those functions that you will frequently use in your code
+
+#for example using yaml files
+ 
